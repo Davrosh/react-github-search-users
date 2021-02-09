7f94d24e012fff68c2c0f0c5c0ae77a1a27198a3
andy <andy@andy>
2021-02-09 13:09:44 +0000

    add build

---
+++ build/precache-manifest.1787b3eb5023ba2ab505d90004a00107.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ded9e550e946c6c0635e9c8b2c858482",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f01c92ab00bcfd340b6",
+    "url": "/static/css/main.ff45c5cb.chunk.css"
+  },
+  {
+    "revision": "bb65cec1a901ec264693",
+    "url": "/static/js/2.d5112333.chunk.js"
+  },
+  {
+    "revision": "af61c9b819ad3fe3b8d9894368d20422",
+    "url": "/static/js/2.d5112333.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f01c92ab00bcfd340b6",
+    "url": "/static/js/main.fd986acb.chunk.js"
+  },
+  {
+    "revision": "864964386b8fbd0a16b5",
+    "url": "/static/js/runtime-main.0fe5df00.js"
+  },
+  {
+    "revision": "20a909842ebf13585654be5bada6a463",
+    "url": "/static/media/login-img.20a90984.svg"
+  },
+  {
+    "revision": "c114fda3847c2b52236772df2f17cd01",
+    "url": "/static/media/preloader.c114fda3.gif"
+  }
+]);
