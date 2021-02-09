3d50a34050f0ad8adf5aa14dd922a69fd4abad36
andy <andy@andy>
2021-02-09 22:54:01 +0000

    add build 5

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from "./pages";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <AuthWrapper>
-      <Router>
+      <Router basename='/'>
         <Switch>
           <PrivateRoute path="/" exact>
             <Dashboard />
